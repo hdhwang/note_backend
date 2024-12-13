2b76f3be1289192f61040c086b501f042b113bc3
hdhwang <hadong0624@naver.com>
2024-12-13 12:58:51 +0900

    uwsgi -> gunicorn으로 변경

--- stop.sh
+++ stop.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE="config.settings.production"
 SCRIPT_PATH=$(dirname $(realpath $0))
-pid_file=$SCRIPT_PATH"/tmp/note.pid"
+
+RUN_PATH=${SCRIPT_PATH}/run
+PID_FILE=${RUN_PATH}/note_drf.pid
 
 source $SCRIPT_PATH/venv/bin/activate
-uwsgi --stop $pid_file
+if [ -f $PID_FILE ]; then
+    # PID 파일이 있으면 종료
+    kill -9 $(cat $PID_FILE)
+    rm -f ${PID_FILE}
+    echo "note_drf stopped successfully."
+fi
 deactivate
