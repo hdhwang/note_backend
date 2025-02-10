9dca09fb89bc3413c6971ec87fc926b98e1c1826
hdhwang <hadong0624@naver.com>
2025-02-11 01:46:49 +0900

    CORS_ORIGIN_ALLOW_ALL 활성화

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,13 +70,13 @@ MIDDLEWARE = [
     'config.custom_exception_handler.ExceptionMiddleware',
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:3000',
-    'https://127.0.0.1:3000',
-    'http://localhost:3000',
-    'https://localhost:3000',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = [
+#     'http://127.0.0.1:3000',
+#     'https://127.0.0.1:3000',
+#     'http://localhost:3000',
+#     'https://localhost:3000',
+# ]
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'config.urls'
